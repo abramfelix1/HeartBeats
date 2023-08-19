ca354000730d63c5d0e6bd92e439fb340b7885f7
Abram Felix <abramfelix1@gmail.com>
2023-08-19 11:52:33 -0700

    setup journal associations

--- backend/db/models/journal.js
+++ backend/db/models/journal.js
@@ -1,26 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Journal extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Journal.belongsTo(models.User, {
+        as: "User",
+        foreignKey: "userId",
+      });
+      Journal.hasOne(models.Playlist, {
+        as: "Playlist",
+        foreignKey: "journalId",
+      });
     }
   }
-  Journal.init({
-    userId: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    content: DataTypes.STRING,
-    image_url: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Journal',
-  });
+  Journal.init(
+    {
+      userId: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      content: DataTypes.STRING,
+      image_url: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Journal",
+    }
+  );
   return Journal;
 };
