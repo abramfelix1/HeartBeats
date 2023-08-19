c9a7f7bc6dc433ce8361c903eadd5a3293ed8f54
Abram Felix <abramfelix1@gmail.com>
2023-08-19 14:12:36 -0700

    add options param to migration files

--- backend/db/migrations/20230819182502-create-song.js
+++ backend/db/migrations/20230819182502-create-song.js
@@ -1,42 +1,52 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Songs", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Songs",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        artists: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.STRING,
+        },
+        img_url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        spotify_url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      artists: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      preview: {
-        type: Sequelize.STRING,
-      },
-      img_url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      spotify_url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Songs");
--- backend/db/migrations/20230819193029-create-playlist-song.js
+++ backend/db/migrations/20230819193029-create-playlist-song.js
@@ -1,39 +1,49 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("PlaylistSongs", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      songId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Songs",
+    await queryInterface.createTable(
+      "PlaylistSongs",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: "CASCADE",
-      },
-      playlistId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Playlists",
+        songId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Songs",
+          },
+          onDelete: "CASCADE",
         },
-        onDelete: "CASCADE",
+        playlistId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Playlists",
+          },
+          onDelete: "CASCADE",
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("PlaylistSongs");
