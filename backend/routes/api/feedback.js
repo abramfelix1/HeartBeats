8d21bcc8855672f7635ba46ccf0348c29c109a0f
Abram Felix <abramfelix1@gmail.com>
2023-08-20 15:39:14 -0700

    add get all feedback of song route

--- backend/routes/api/feedback.js
+++ backend/routes/api/feedback.js
@@ -3,7 +3,7 @@ const sequelize = require("sequelize");
 const { Op } = require("sequelize");
 const { requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
-const { Song, Userfeedback } = require("../../db/models");
+const { Song, UserFeedback } = require("../../db/models");
 
 const router = express.Router();
 
@@ -16,10 +16,46 @@ router.get("/session", requireAuth, async (req, res, next) => {
   });
 
   if (!userFeedback.length) {
-    res.json({ feedback: [] });
+    return res.json({ feedback: [] });
   }
 
-  res.json({ feedback: userFeedback });
+  res.json({ userFeedback: userFeedback });
+});
+
+/* GET ALL FEEDBACK OF SONG */
+router.get("/:songId", async (req, res, next) => {
+  const { songId } = req.params;
+
+  const song = await Song.findByPk(songId);
+
+  if (!song) {
+    return next({
+      errors: { song: "Song could not be found", status: 404 },
+    });
+  }
+
+  const songFeedback = await UserFeedback.findAll({
+    where: { songId },
+    attributes: [
+      "id",
+      "userId",
+      "feedbackType",
+      [sequelize.fn("COUNT", sequelize.col("id")), "totalFeedbacks"],
+      [sequelize.fn("SUM", sequelize.col("like")), "totalLikes"],
+    ],
+    group: ["UserFeedback.id", "UserFeedback.feedbackType"],
+    include: [
+      {
+        model: Song,
+      },
+    ],
+  });
+
+  if (!songFeedback.length) {
+    return res.json({ songFeedback: [] });
+  }
+
+  res.json({ songFeedback: songFeedback });
 });
 
 module.exports = router;
