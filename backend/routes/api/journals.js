d21f612a950658cc7798f5459101c841b2da4b2c
Abram Felix <abramfelix1@gmail.com>
2023-08-20 09:39:11 -0700

    add get journal by id

--- backend/routes/api/journals.js
+++ backend/routes/api/journals.js
@@ -3,7 +3,7 @@ const sequelize = require("sequelize");
 const { Op } = require("sequelize");
 const { requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
-const { Journal, Playlist } = require("../../db/models");
+const { Journal, Playlist, Song } = require("../../db/models");
 
 const router = express.Router();
 
@@ -27,6 +27,36 @@ router.get("/session", requireAuth, async (req, res, next) => {
   }
 
   res.json({ Journals: journals });
+});
+
+/* GET JOURNALS BY ID */
+router.get("/:id", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const journalId = req.params.id;
+
+  const journal = await Journal.findOne({
+    where: { id: journalId, userId: user.dataValues.id },
+    include: [
+      {
+        model: Playlist,
+        as: "Playlist",
+        include: [
+          {
+            model: Song,
+            as: "Songs",
+          },
+        ],
+      },
+    ],
+  });
+
+  if (!journal) {
+    return next({
+      errors: { journal: "Journal could not be found", status: 404 },
+    });
+  }
+
+  res.json({ Journal: journal });
 });
 
 module.exports = router;
