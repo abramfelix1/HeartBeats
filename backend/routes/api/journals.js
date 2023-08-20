1fc007768af82a7b50eb8a732ce74ac4408c66f6
Abram Felix <abramfelix1@gmail.com>
2023-08-20 09:44:57 -0700

    add update and delete journal

--- backend/routes/api/journals.js
+++ backend/routes/api/journals.js
@@ -35,7 +35,7 @@ router.get("/:id", requireAuth, async (req, res, next) => {
   const journalId = req.params.id;
 
   const journal = await Journal.findOne({
-    where: { id: journalId, userId: user.dataValues.id },
+    where: { id: journalId },
     include: [
       {
         model: Playlist,
@@ -57,6 +57,58 @@ router.get("/:id", requireAuth, async (req, res, next) => {
   }
 
   res.json({ Journal: journal });
+});
+
+/* UPDATE JOURNAL BY ID */
+router.put("/:id", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const journalId = req.params.id;
+
+  const journal = await Journal.findOne({
+    where: { id: journalId },
+  });
+
+  if (!journal) {
+    return next({
+      errors: { journal: "Journal could not be found", status: 404 },
+    });
+  }
+
+  if (journal.userId !== user.dataValues.id) {
+    return next({
+      errors: { journal: "Unauthorized Access", status: 401 },
+    });
+  }
+
+  const updatedJournal = await journal.update(req.body);
+
+  res.json({ UpdatedJournal: updatedJournal });
+});
+
+/* DELETE JOURNAL BY ID */
+router.put("/:id", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const journalId = req.params.id;
+
+  const journal = await Journal.findOne({
+    where: { id: journalId },
+  });
+
+  if (!journal) {
+    return next({
+      errors: { journal: "Journal could not be found", status: 404 },
+    });
+  }
+
+  if (journal.userId !== user.dataValues.id) {
+    return next({
+      errors: { journal: "Unauthorized Access", status: 401 },
+    });
+  }
+
+  await journal.destroy();
+
+  res.json({ message: "Journal deleted successfully" });
 });
 
 module.exports = router;
