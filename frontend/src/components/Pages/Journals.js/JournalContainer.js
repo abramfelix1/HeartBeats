c1f6d7e5d1756705738f276e8375f641a3c8020f
Abram Felix <abramfelix1@gmail.com>
2023-08-27 06:41:35 -0700

    add react-tooltips for quill toolbar tools

--- frontend/src/components/Pages/Journals.js/JournalContainer.js
+++ frontend/src/components/Pages/Journals.js/JournalContainer.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Tooltip } from "react-tooltip";
 import JournalNav from "./JournalNav";
 
 export default function JournalContainer() {
@@ -18,6 +19,7 @@ export default function JournalContainer() {
       [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
     ],
   };
+
   const submitHandler = (e) => {
     e.preventDefault();
     const quill = quillRef.current.getEditor();
@@ -28,11 +30,47 @@ export default function JournalContainer() {
     console.log(formattedContent);
   };
 
+  useEffect(() => {
+    const tooltips = {
+      ".ql-bold": "Make text bold",
+      ".ql-italic": "Italicize text",
+      ".ql-underline": "Underline text",
+      ".ql-font": "Choose font",
+      ".ql-size": "Set font size",
+      ".ql-color": "Change text color",
+      ".ql-background": "Change text background color",
+      ".ql-align": "Align text",
+      '.ql-list[value="bullet"]': "Bullet list",
+      '.ql-list[value="ordered"]': "Numbered list",
+      '.ql-indent[value="-1"]': "Decrease indent",
+      '.ql-indent[value="+1"]': "Increase indent",
+    };
+
+    if (quillRef.current) {
+      const editor = quillRef.current.getEditor();
+      const toolbar = editor.getModule("toolbar");
+      console.log("TOOLBAR:", toolbar);
+      for (let button in tooltips) {
+        let btns = toolbar.container.querySelectorAll(button);
+        btns.forEach((btn) => {
+          btn.setAttribute("data-tooltip-id", "toolbar-tooltip");
+          btn.setAttribute("data-tooltip-content", tooltips[button]);
+        });
+      }
+    }
+  }, []);
+
   return (
     <div className="flex mx-2 my-2 w-[50%] bg-baby-powder rounded-3xl">
       <JournalNav />
       <div className="w-full p-10">
         <form onSubmit={submitHandler}>
+          <Tooltip
+            place="top"
+            type="dark"
+            effect="solid"
+            id="toolbar-tooltip"
+          />
           <ReactQuill
             modules={modules}
             ref={quillRef}
