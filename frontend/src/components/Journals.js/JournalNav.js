5b10cfc657b5d40daba60bead6afba4d84e63dc7
Abram Felix <abramfelix1@gmail.com>
2023-08-28 15:20:03 -0700

    update

--- frontend/src/components/Journals.js/JournalNav.js
+++ frontend/src/components/Journals.js/JournalNav.js
@@ -83,11 +83,9 @@ export default function JournalNav() {
                 <div
                   className="flex flex-row gap-x-2 items-center"
                   key={journal.id}
+                  onClick={() => setJournal(journal)}
                 >
-                  <p
-                    className="whitespace-nowrap overflow-hidden text-ellipsis sm:w-[90px] md:w-[110px] lg:w-[130px] xl:w-[140px] 2xl:w-[150px] hover:cursor-pointer"
-                    onClick={() => setJournal(journal)}
-                  >
+                  <p className="whitespace-nowrap overflow-hidden text-ellipsis sm:w-[90px] md:w-[110px] lg:w-[130px] xl:w-[140px] 2xl:w-[150px] hover:cursor-pointer">
                     {journal.name}
                   </p>
                   <PiTrash
--- frontend/src/components/ModalPages/DeleteConfirmation.js
+++ frontend/src/components/ModalPages/DeleteConfirmation.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useDispatch } from "react-redux";
+import { JournalContext } from "../../context/journalContext";
+import { deleteJournal } from "../../store/journals";
 
-export default function DeleteConfirmation() {
+export default function DeleteConfirmation({ id }) {
   const dispatch = useDispatch();
-  return <div className="flex w-10 h-10 bg-blue-400">DeleteConfirmation</div>;
+  const { journal } = useContext(JournalContext);
+
+  const deleteJournalHandler = () => {
+    dispatch(deleteJournal(journal.id));
+  };
+
+  return (
+    <div className="flex w-10 h-10 bg-blue-400">DELETE {journal.name}?</div>
+  );
 }
