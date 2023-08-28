089ae15939379dc6e8b3e6b2b21486efcf5ec021
Abram Felix <abramfelix1@gmail.com>
2023-08-27 18:31:44 -0700

    map journals to journal nav and group by dates

--- frontend/src/components/Pages/Journals.js/JournalNav.js
+++ frontend/src/components/Pages/Journals.js/JournalNav.js
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllJournals } from "../../../store/journals";
 
 export default function JournalNav() {
-  return <div className="h-full w-64 bg-[#ececf5] rounded-l-3xl "></div>;
+  const dispatch = useDispatch();
+  const journals = useSelector((state) => Object.values(state.journals));
+
+  const sortedJournals = [...journals].sort(
+    (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
+  );
+
+  const groupedJournals = sortedJournals.reduce((acc, journal) => {
+    const today = new Date();
+    const journalDate = new Date(journal.updatedAt);
+    let label;
+
+    if (
+      journalDate.getDate() === today.getDate() &&
+      journalDate.getMonth() === today.getMonth() &&
+      journalDate.getFullYear() === today.getFullYear()
+    ) {
+      label = "Today";
+    } else if (
+      journalDate.getDate() === today.getDate() - 1 &&
+      journalDate.getMonth() === today.getMonth() &&
+      journalDate.getFullYear() === today.getFullYear()
+    ) {
+      label = "Yesterday";
+    } else {
+      label = journalDate.toDateString();
+    }
+
+    if (!acc[label]) {
+      acc[label] = [];
+    }
+
+    acc[label].push(journal);
+    return acc;
+  }, {});
+
+  useEffect(() => {
+    dispatch(getAllJournals());
+  }, []);
+
+  return (
+    <div className="h-full w-64 bg-[#ececf5] rounded-l-3xl ">
+      <div className="flex flex-col gap-y-3 items-center pt-20">
+        {Object.entries(groupedJournals).map(([date, journalsOnDate]) => (
+          <div key={date}>
+            <h2>{date}</h2>
+            {journalsOnDate.map((journal) => (
+              <div key={journal.id}>
+                <p>{journal.name}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
