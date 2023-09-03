90eb43dde1544641483b94bd1f2e968c36f38faa
Abram Felix <abramfelix1@gmail.com>
2023-09-03 14:52:08 -0700

    add more styling

--- frontend/src/components/Songs/Playlist.js
+++ frontend/src/components/Songs/Playlist.js
@@ -9,6 +9,7 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import { JournalContext } from "../../context/journalContext";
 import { PlaylistContext } from "../../context/playlistContext";
+import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 export default function Playlist() {
   const dispatch = useDispatch();
@@ -34,33 +35,38 @@ export default function Playlist() {
           <p>{playlist.id}</p>
         </div>
         <div className="flex flex-row items-center header-row">
-          <div className="w-7 text-center">#</div>
+          <div className="mx-2 text-center">#</div>
           <div className="flex flex-1 items-start">
-            <div className="ml-3">Title</div>
+            <div className="ml-2">Title</div>
           </div>
           <div className="flex-1">Album</div>
         </div>
-        <div className="playlist flex flex-col my-2 overflow-y-auto gap-y-2">
+        <div className="playlist flex flex-col my-3 overflow-y-auto gap-y-2">
           {playlistSongs &&
             playlistSongs.map((song, index) => (
-              <div
-                className="flex flex-row items-center"
-                key={song.id}
-                data-id={song.id}
-              >
-                <div className="w-10 text-center">{index + 1}</div>
+              <div className="flex flex-row" key={song.id} data-id={song.id}>
+                <div className="mx-3 text-center">{index + 1}</div>
                 <div className="flex flex-1 items-start">
                   <img
-                    src={"https://i.imgur.com/0OXyb26.jpeg"}
+                    src={
+                      "https://i.scdn.co/image/ab67616d00001e02a1214ad1ca57685349932cea"
+                    }
                     alt="Album Cover"
-                    className="w-14 h-14 mr-5"
+                    className="w-20 h-20 mr-3"
                   />
                   <div className="flex flex-col gap-y-1">
-                    <div>{song.name}</div>
-                    <div className="song-artist">{song.artists}</div>
+                    <div>
+                      Be Who You Are (Real Magic) (feat. JID, NewJeans & Camilo)
+                    </div>
+                    <div className="">
+                      Jon Batiste ft. JID, NewJeans, Camilo
+                    </div>
                   </div>
                 </div>
-                <div className="flex-1">{song.album}</div>
+                <div className="flex-1 ml-10">Be Who You Are (Real Magic)</div>
+                <div className="flex mr-5 ">
+                  <IoRemoveCircleOutline className="text-xl" />
+                </div>
               </div>
             ))}
         </div>
