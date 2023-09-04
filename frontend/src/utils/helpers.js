deed097b62d948b0a2aeab4556c64b82634448e8
Abram Felix <abramfelix1@gmail.com>
2023-09-03 23:10:13 -0700

    create debounce helper

---
+++ frontend/src/utils/helpers.js
@@ -0,0 +1,11 @@
+export const debounce = (func, time) => {
+  let timeout;
+  return function (...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, time);
+  };
+};
