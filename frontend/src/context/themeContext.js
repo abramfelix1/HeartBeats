690aab02449e0a66644cc19774427da91227ab55
Abram Felix <abramfelix1@gmail.com>
2023-09-04 04:28:07 -0700

    setup dark theme colors

--- frontend/src/context/themeContext.js
+++ frontend/src/context/themeContext.js
@@ -11,9 +11,11 @@ export const ThemeProvider = ({ children }) => {
 
   useEffect(() => {
     if (theme === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
     } else {
+      document.documentElement.setAttribute("data-theme", "light");
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
         "dodger-blue": "#3590F3",
         "azure-blue": "#508bc3",
         "baby-powder": "#fffffc",
+        "bkg-primary": "hsl(var(--color-primary-100) / <alpha-value>)",
+        "bkg-body": "hsl(var(--color-surface-mixed-100) / <alpha-value>)",
+        "bkg-card": "hsl(var(--color-surface-mixed-200) / <alpha-value>)",
+        "bkg-button": "hsl(var(--color-surface-mixed-300) / <alpha-value>)",
+        "bkg-nav": "hsl(var(--color-surface-mixed-400) / <alpha-value>)",
+        "bkg-modal": "hsl(var(--color-surface-mixed-500) / <alpha-value>)",
+        "bkg-text": "hsl(var(--color-surface-mixed-600) / <alpha-value>)",
       },
       backgroundImage: {
         login: "url('./images/login-background4.png')",
