6d320bd73a36a996f65de4716b678cb97a32f987
Abram Felix <abramfelix1@gmail.com>
2023-09-04 15:18:02 -0700

    make columns more consistent and show elipses

--- frontend/src/components/Songs/Playlist.js
+++ frontend/src/components/Songs/Playlist.js
@@ -57,7 +57,7 @@ export default function Playlist() {
             className="bg-bkg-card p-3 border-none rounded-3xl focus:outline-none font-semibold w-full"
           />
         </div>
-        <div className="grid grid-cols-[16px,4fr,2fr,minmax(120px,1fr)] gap-x-4 items-center px-4 border-b-[1px] border-b-[#cccccc]">
+        <div className="grid grid-cols-[16px,4fr,3fr,0.5fr] gap-x-4 items-center px-4 border-b-[1px] border-b-[#cccccc]">
           <div className="text-center">#</div>
           <div className="ml-2">Title</div>
           <div className="">Album</div>
@@ -67,12 +67,12 @@ export default function Playlist() {
           {playlistSongs &&
             playlistSongs.map((song, index) => (
               <div
-                className="grid grid-cols-[16px,4fr,2fr,minmax(120px,1fr)] gap-4 items-center px-4 border rounded h-14 border-transparent relative"
+                className="grid grid-cols-[16px,4fr,3fr,0.5fr] gap-4 items-center px-4 border rounded h-14 border-transparent relative"
                 key={song.id}
                 data-id={song.id}
               >
                 <div className="text-center">{index + 1}</div>
-                <div className="flex items-start w-full">
+                <div className="flex items-start items-center w-full min-w-0">
                   <img
                     src={
                       "https://i.scdn.co/image/ab67616d00001e02a1214ad1ca57685349932cea"
@@ -80,7 +80,7 @@ export default function Playlist() {
                     alt="Album Cover"
                     className="w-10 h-10 mr-3"
                   />
-                  <div className="flex flex-col gap-y-1 pr-2">
+                  <div className="flex flex-col gap-y-1 pr-2 w-full truncate">
                     <div className="truncate ">
                       Be Who You Are (Real Magic) (feat. JID, NewJeans & Camilo)
                     </div>
@@ -89,7 +89,10 @@ export default function Playlist() {
                     </div>
                   </div>
                 </div>
-                <div className="">Be Who You Are (Real Magic)</div>
+                <div className="truncate">
+                  {" "}
+                  Be Who You Are (Real Magic) (feat. JID, NewJeans & Camilo)
+                </div>
                 <button className="flex justify-end mr-5">
                   <IoRemoveCircleOutline className="text-xl" />
                 </button>
