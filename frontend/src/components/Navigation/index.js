424d5086e99036a64ea7f4d99ff958d335793c77
Abram Felix <abramfelix1@gmail.com>
2023-09-06 01:35:21 -0700

    style nav

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -13,12 +13,13 @@ import { JournalContext } from "../../context/journalContext";
 import { resetJournalsActions } from "../../store/journals";
 import { PlaylistContext } from "../../context/playlistContext";
 import { ThemeContext } from "../../context/themeContext";
-// import journalIcon from "../../images/journal.svg";
-// import playlistIcon from "../../images/playlist-folder.svg";
-// import settingsIcon from "../../images/cog.svg";
-// import logInIcon from "../../images/log-in.svg";
-// import logOutIcon from "../../images/log-out.svg";
 import { ReactComponent as JournalIcon } from "../../images/icons/outline/journal.svg";
+import { ReactComponent as PlaylistIcon } from "../../images/icons/outline/playlist-folder.svg";
+import { ReactComponent as SettingsIcon } from "../../images/icons/outline/cog.svg";
+import { ReactComponent as LogInIcon } from "../../images/icons/outline/login.svg";
+import { ReactComponent as LogOutIcon } from "../../images/icons/outline/logout.svg";
+import { ReactComponent as SunIcon } from "../../images/icons/outline/sun.svg";
+import { ReactComponent as MoonIcon } from "../../images/icons/outline/moon.svg";
 
 function Navigation({ isLoaded, navHovered, ...props }) {
   const dispatch = useDispatch();
@@ -42,61 +43,27 @@ function Navigation({ isLoaded, navHovered, ...props }) {
   };
 
   return (
-    <div
-      className={`bg-bkg-nav flex flex-col ml-2 my-2 mr-2 items-center relative rounded-3xl ${
-        isCollapsed ? "px-1" : "px-1"
-      }
-      `}
-      {...props}
-    >
-      <button
-        className="bg-bkg-nav absolute h-[85%] w-[5px] right-0  top-[7.5%] rounded-3xl opacity-0 user-select: none hover:opacity-0 "
-        onClick={collapseClickHandler}
-      />
-      <div className="flex flex-col flex-grow justify center items-center">
-        <NavLink exact to="/" className="py-5">
-          <img
-            src={logo}
-            alt="logo"
-            className={`${isCollapsed ? "w-[50px]" : "w-[75px]"}`}
-          />
-        </NavLink>
-        <div className="space-y-8 my-8">
-          {/* {sessionUser && (
-            <div
-              className="flex gap-x-2 justify-center items-center hover:cursor-pointer"
-              data-tooltip-id="nav-tooltip"
-              data-tooltip-content="Profile"
-            >
-              <AiOutlineUser className="text-[35px]" />
-              {!isCollapsed && <p className="select-none">Profile</p>}
-            </div>
-          )} */}
-          {sessionUser && (
-            <div
-              className="flex gap-x-2 justify-center items-center  hover:cursor-pointer"
-              data-tooltip-id="nav-tooltip"
-              data-tooltip-content="Journal"
-              onClick={toggleJournalPage}
-            >
-              <JournalIcon className="fill" />
-              {/* <img src={journalIcon} alt="Journal Icon" /> */}
-              {!isCollapsed && <p className="select-none">Journal</p>}
-            </div>
-          )}
-          {sessionUser && (
-            <div
-              className="flex gap-x-2 justify-center items-center  hover:cursor-pointer"
-              data-tooltip-id="nav-tooltip"
-              data-tooltip-content="Music"
-            >
-              {/* <img src={playlistIcon} alt="Journal Icon" /> */}
-              {!isCollapsed && <p className="select-none">Music</p>}
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="space-y-8 mb-5">
+    <div className=" flex w-full justify-end m-10">
+      <div className="bg-bkg-nav flex flex-col items center gap-3 h-fit w-fit p-2 rounded-lg">
+        {sessionUser && (
+          <div
+            className=" flex gap-x-2 justify-center items-center  hover:cursor-pointer"
+            data-tooltip-id="nav-tooltip"
+            data-tooltip-content="Journal"
+            onClick={toggleJournalPage}
+          >
+            <JournalIcon className="w-12 fill-txt-1" />
+          </div>
+        )}
+        {sessionUser && (
+          <div
+            className="flex gap-x-2 justify-center items-center  hover:cursor-pointer"
+            data-tooltip-id="nav-tooltip"
+            data-tooltip-content="Music"
+          >
+            <PlaylistIcon className="w-12 fill-txt-1" />
+          </div>
+        )}
         {sessionUser && (
           <div
             className="flex gap-x-2 justify-center items-center  hover:cursor-pointer"
@@ -109,21 +76,24 @@ function Navigation({ isLoaded, navHovered, ...props }) {
                 <input
                   type="checkbox"
                   id="check"
-                  className="mode-checkbox opacity-0 absolute"
+                  className="mode-checkbox opacity-0 absolute "
                   onChange={toggleDark}
                   checked={theme === "dark"}
                 />
                 <label
                   for="check"
-                  className="mode-label relative flex justify-between items-center p-[5px] h-[26px] w-[60px] bg-bkg-card rounded-2xl cursor-pointer"
+                  className="mode-label relative flex justify-between items-center p-1 h-12 w-[48px] rounded-lg border-2 border-txt-1 cursor-pointer"
                 >
-                  <FaRegMoon className=" text-bkg-text" />
-                  <BsFillBrightnessHighFill className=" text-bkg-text" />
-                  <div className="mode-ball absolute bg-bkg-text top-[2px] left-[2px] w-[22px] h-[22px] rounded-full translate-x-0 transition-transform duration-150 ease-linear"></div>
+                  <MoonIcon className="w-8 fill-txt-1" />
+                  <SunIcon className="w-8 fill-txt-1" />
+                  <div
+                    className={`mode-ball absolute bg-txt-1 left-[0px] w-[50%] h-full translate-x-0 transition-transform duration-150 ease-linear ${
+                      theme === "dark" ? "rounded-r-md" : "rounded-l-md"
+                    } `}
+                  ></div>
                 </label>
               </>
             )}
-            {!isCollapsed && <p className="select-none">Theme</p>}
           </div>
         )}
         {sessionUser && (
@@ -132,8 +102,7 @@ function Navigation({ isLoaded, navHovered, ...props }) {
             data-tooltip-id="nav-tooltip"
             data-tooltip-content="Settings"
           >
-            {/* <img src={settingsIcon} alt="Settings Icon" />{" "} */}
-            {!isCollapsed && <p className="select-none">Settings</p>}
+            <SettingsIcon className="w-12 fill-txt-1" />
           </div>
         )}
         {sessionUser ? (
@@ -146,8 +115,7 @@ function Navigation({ isLoaded, navHovered, ...props }) {
             data-tooltip-id="nav-tooltip"
             data-tooltip-content="Logout"
           >
-            {/* <img src={logOutIcon} alt="Log Out Icon" /> */}
-            {!isCollapsed && <p className="select-none">Logout</p>}
+            <LogOutIcon className="w-12 fill-txt-1" />
           </div>
         ) : (
           <NavLink
@@ -156,20 +124,17 @@ function Navigation({ isLoaded, navHovered, ...props }) {
             data-tooltip-id="nav-tooltip"
             data-tooltip-content="Login"
           >
-            {/* <img src={logInIcon} alt="Log In Icon" /> */}
-            {!isCollapsed && <p className="select-none">Login</p>}
+            <LogInIcon className="w-12 fill-txt-1" />
           </NavLink>
         )}
       </div>
-      {isCollapsed && (
-        <Tooltip
-          className="mx-1 z-10"
-          place="right"
-          type="dark"
-          effect="solid"
-          id="nav-tooltip"
-        />
-      )}
+      <Tooltip
+        className="mx-1 z-10"
+        place="left"
+        type="dark"
+        effect="solid"
+        id="nav-tooltip"
+      />
     </div>
   );
 }
