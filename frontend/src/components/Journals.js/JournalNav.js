79fad37ad3cf510690afafbdfca6e95ef96a7207
Abram Felix <abramfelix1@gmail.com>
2023-09-06 17:33:54 -0700

    conver time

--- frontend/src/components/Journals.js/JournalNav.js
+++ frontend/src/components/Journals.js/JournalNav.js
@@ -58,7 +58,7 @@ export default function JournalNav() {
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const day = date.getDate().toString().padStart(2, "0");
 
-    const hour = date.getHours() % 12 || 12; // Convert to 12-hour format
+    const hour = date.getHours() % 12 || 12;
     const minute = date.getMinutes().toString().padStart(2, "0");
     const ampm = date.getHours() >= 12 ? "PM" : "AM";
 
