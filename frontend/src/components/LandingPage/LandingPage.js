e00e802c24458ce6d0d4241f4c6a95503083dc6e
Abram Felix <abramfelix1@gmail.com>
2023-09-10 23:54:59 -0700

    add logo

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "../../images/heartBeatLogo.png";
+import logo from "../../images/heartBeatLogoName.png";
 
 export default function LandingPage() {
   return (
-    <div className="flex w-screen h-screen bg-blue-100">
+    <div className="flex w-screen h-screen bg-white">
       <div className="flex w-full">
-        <div className="flex h-24 w-full items-center bg-blue-200">
-          <img className="w-24 h-24 p-4" src={logo} alt="logo" />
-          <p className="text-5xl p-2">HeartBeats</p>
+        <div className="flex items-start w-full  p-4">
+          <img className="w-96 " src={logo} alt="logo" />
         </div>
       </div>
     </div>
